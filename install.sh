60a7cb90578224e97ee81b6b4c39c6ece5b6c3bc
Rob Howard <damncabbage@gmail.com>
2011-08-19 21:54:23 +1000

    My very own special-snowflake symlink install script.

---
+++ install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# Symlinks the contents of the dotfiles path into 
+#
+
+function info() {
+	echo -e "\e[0;32m **\e[m $*"
+}
+
+function error() {
+	echo -e "\e[1;31m !!\e[m $*"
+}
+
+function action() {
+	CODE=$1
+	shift
+	if [ $CODE -gt 0 ]; then
+		error $* " (Failed with code $CODE)"
+	else
+		info $*
+	fi
+}
+
+
+# Make sure we're in the same directory as the script
+pushd `dirname $0` > /dev/null
+
+	# Can anyone say HACK?
+	FILES=".[A-Za-z0-9_]* bin/*"
+	TARGETDIR=$HOME
+
+	for FILE in $FILES
+	do
+		# HACK: Ignore our repo .git if this is not a bare repo.
+		if [ $FILE == ".git" ] || [ $FILE == ".gitignore" ]; then
+			continue
+		fi
+
+		if [ -f ~/"$FILE" ] || [ -d ~/"$FILE" ]; then
+			if [ -L ~/"$FILE" ]; then
+				info "Already symlinked: $FILE"
+			else
+				error "Already present and is a regular file: $FILE"
+			fi
+
+		else 
+			DIR=`dirname "$FILE"`
+			if [ ! -d "$TARGETDIR/$DIR" ]; then
+				mkdir -p "$TARGETDIR/$DIR"
+				action $? "Created directory: $DIR"
+			fi
+
+			ln -s "`pwd`/$FILE" "$TARGETDIR/$DIR/"
+			action $? "Linked: $FILE"
+		fi
+	done
+
+popd > /dev/null
