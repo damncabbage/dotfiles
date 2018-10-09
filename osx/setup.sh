3c993966f3e87553e8004144b441844a473fe94b
Rob Howard <rob@robhoward.id.au>
2018-10-09 11:45:22 +1100

    Updates macOS setup script to match current settings.

--- osx/setup.sh
+++ osx/setup.sh
@@ -18,9 +18,10 @@ brew cask install \
   vlc \
   flux \
   sim-daltonism \
-  focuswriter
+  focuswriter \
+  virtualbox \
+  karabiner
   # aws-vault
-  # virtualbox
   # tunnelblick
 
 echo "Install slowquitapps?"
@@ -30,29 +31,62 @@ fi
 
 cat <<-EOF
   Manually install:
-  - iTerm2 <http://www.iterm2.com/#/section/downloads>
   - GHC+Cabal <http://ghcformacosx.github.io/>
   - Tunnelblick <https://code.google.com/p/tunnelblick/wiki/DownloadsEntry#Tunnelblick_Beta_Release>
-  - VLC <http://www.videolan.org/vlc/index.html>
-  - Caffeine <http://lightheadsw.com/caffeine/>
 EOF
 
 # Pomodoro
 gem install thyme
 
-# These make MacOS randomly shut down after being suspended for a while sometimes. :|
-#sudo pmset -a destroyfvkeyonstandby 1
-#sudo pmset -a standbydelay 300 # 5-minute hibernate countdown
-
-# Make HiDPI mode available for high-res external monitors
-# sudo defaults write /Library/Preferences/com.apple.windowserver.plist DisplayResolutionEnabled -bool true
-
-#wget http://download.virtualbox.org/virtualbox/5.0.10/VirtualBox-5.0.10-104061-OSX.dmg > /tmp/virtualbox.dmg
-#open /tmp/virtualbox.dmg
-
+# CPU Temp tool
 mkdir -p ~/build
 (
   git clone https://github.com/lavoiesl/osx-cpu-temp ~/build/osx-cpu-temp;
   cd ~/build/osx-cpu-temp;
   git checkout git22a86f51fb1c421bafceb0aebc009bd7337982f8 && make && ./osx-cpu-temp && mv -i ./osx-cpu-temp ~/bin/
+)
+
+
+### macOS settings
+(
+  # Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
+  defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+  # Expand save panel by default
+  defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
+
+  # Expand print panel by default
+  defaults write -g PMPrintingExpandedStateForPrint -bool true
+
+  # Disable auto-correct
+  defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+  # Automatically open a new Finder window when a volume is mounted
+  defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+  defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+
+  # Avoid creating .DS_Store files on network volumes
+  defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+  # Disable Safari’s thumbnail cache for History and Top Sites
+  defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+
+  # Enable Safari’s debug menu
+  defaults write com.apple.Safari IncludeDebugMenu -bool true
+
+  # Show the ~/Library folder
+  chflags nohidden ~/Library
+
+  # Dim hidden apps
+  defaults write com.apple.dock showhidden -bool true
+
+  # These make MacOS randomly shut down after being suspended for a while sometimes. :|
+  #sudo pmset -a destroyfvkeyonstandby 1
+  #sudo pmset -a standbydelay 300 # 5-minute hibernate countdown
+
+  # Make HiDPI mode available for high-res external monitors
+  # sudo defaults write /Library/Preferences/com.apple.windowserver.plist DisplayResolutionEnabled -bool true
+
+  # Restart settings-affected applications
+  for app in Safari Finder Dock Mail; do killall "$app"; done
 )
