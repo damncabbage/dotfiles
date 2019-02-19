5b8b4c620efc9950b1dee2a9bffba8e055a38275
Rob Howard <rob@robhoward.id.au>
2019-02-19 17:11:31 +1100

    Improve the yelling script.

--- bin/countdown_and.sh
+++ bin/countdown_and.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -eu
 
-[[ -z "$2" ]] && (echo "Usage: $0 minutes command-to-run"; exit 1);
+[[ -z "${2:-}" ]] && (
+  echo "Usage: $0 minutes command-to-run";
+  echo "eg.    $0 5 'say \"do the fucking thing goddamnit\"'";
+  exit 1
+);
 
-SECS=$(($1 * 60))
-while [ "$SECS" -gt 0 ]; do
+SECS=$(($1 * 60));
+while [[ "$SECS" > 0 ]]; do
    echo -ne "$SECS\033[0K\r";
    sleep 1;
    SECS=$((SECS - 1));
