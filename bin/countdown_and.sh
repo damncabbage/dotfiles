275bc366438d3582f62d6af318a1f1058fcfed43
Rob Howard <rob@robhoward.id.au>
2020-04-27 12:14:20 +1000

    Nicer output for countdown_and.sh

--- bin/countdown_and.sh
+++ bin/countdown_and.sh
@@ -8,9 +8,10 @@
 
 SECS=$(($1 * 60));
 while [[ "$SECS" > 0 ]]; do
-   echo -ne "$SECS\033[0K\r";
+   echo -ne "$SECS seconds remaining ($((SECS / 60)) minutes, $((SECS % 60)) seconds).\033[0K\r"
    sleep 1;
    SECS=$((SECS - 1));
 done
 
+echo
 bash -c "$2"
