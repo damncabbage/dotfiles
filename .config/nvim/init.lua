cfc74b7ddf2f339dc9c574f56ced283b2462afdb
Robin Howard <robin@rhoward.id.au>
2022-11-30 12:21:45 +1100

    vim: Set up ensure-installed LSP list for Rust, Ruby and TS.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -350,7 +350,11 @@ require('mason').setup()
 require('mason-lspconfig').setup()
 
 local lspconfig = require("lspconfig")
-require("mason-lspconfig").setup_handlers {
+local masonLspConfig = require("mason-lspconfig")
+masonLspConfig.setup {
+  ensure_installed = { 'rust_analyzer', 'solargraph', 'tsserver' },
+}
+masonLspConfig.setup_handlers {
   function (server_name)
     lspconfig[server_name].setup {}
   end
